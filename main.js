9c17b71197a2b920c7a99959565ad836e9f14761
lukas_adom <63719701+LukasAdom@users.noreply.github.com>
2024-07-07 12:00:29 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,8 @@
 
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 4000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+import dns from 'node:dns'
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+dns.setDefaultResultOrder('verbatim')
 
 
 /* //////// */
